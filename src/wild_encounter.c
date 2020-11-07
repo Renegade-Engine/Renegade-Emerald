0b41fd85edb1119c25145a8e5da74fcf1f9392c1
rglezg <renierglez97@gmail.com>
2020-11-07 06:51:19 +0100

    Changed all wild battles to double.

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -972,11 +972,5 @@ bool8 TryDoDoubleWildBattle(void)
 {
     if (GetSafariZoneFlag() || GetMonsStateToDoubles() != PLAYER_HAS_TWO_USABLE_MONS)
         return FALSE;
-    else if (B_FLAG_FORCE_DOUBLE_WILD != 0 && FlagGet(B_FLAG_FORCE_DOUBLE_WILD))
-        return TRUE;
-    #if B_DOUBLE_WILD_CHANCE != 0
-    else if ((Random() % 100) + 1 < B_DOUBLE_WILD_CHANCE)
-        return TRUE;
-    #endif
-    return FALSE;
+    return TRUE;
 }
