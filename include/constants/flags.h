ce6fcc9b809437eb20bf0c03ea5be25c949b78a2
eglezg <73887377+eglezg@users.noreply.github.com>
2021-06-03 17:33:56 -0400

    Add Kecleon & Sudowoodo
    - Added Kecleon at Route 121 to block Aqua Team event
    - Added Sudowoodo at Route 118 to block pass between Mauville, Route 119 and Route 123

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,8 +43,8 @@
 
 #define FLAG_HIDE_SS_TIDAL_CORRIDOR_RIVAL    0x20
 #define FLAG_HIDE_SS_TIDAL_CABIN_RIVAL    0x21
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define FLAG_HIDE_ROUTE_118_SUDOWOODO    0x22
+#define FLAG_HIDE_ROUTE_121_KECLEON_1    0x23
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
